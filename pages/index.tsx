96cc99216a5731d850d04549ba45e70474a0f780
Oooooiii <hearong.200@gmail.com>
2022-01-22 13:26:25 +0700

    infer get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import type { InferGetStaticPropsType } from "next";
 
-export default function Home() {
+export async function getStaticProps() {
+  const products = [1, 2, 3, 4];
 
-  
-  return (
-    <div>Hello World</div>
-  )
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
 }
